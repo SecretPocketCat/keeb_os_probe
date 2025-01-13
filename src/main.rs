cf071f530bbc9695ef6ad01872fd4f964cc09a29
Jiří H. <jiri@bacehub.de>
2025-01-13 23:07:47 +0100

    feat: long running service that checks for the boards being added

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,14 @@
     windows_subsystem = "windows"
 )]
 
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, thread, time::Duration};
 
 use anyhow::Context;
+use rusb::UsbContext;
 use serde::Deserialize;
 
 const HID_USAGE: u16 = 0x61;
 const HID_USAGE_PAGE: u16 = 0xFF60;
-
-#[derive(Debug, Deserialize)]
-struct Config {
-    keyboards: HashMap<String, KeyboardConfig>,
-}
-
-#[derive(Debug, Deserialize)]
-struct KeyboardConfig {
-    vendor_id: u16,
-    product_id: u16,
-}
 
 /// [QMK OS enum](https://github.com/qmk/qmk_firmware/blob/26f898c8a538b808cf506f558a9454f7f50e3ba6/quantum/os_detection.h#L23)
 #[cfg(target_os = "linux")]
@@ -33,29 +23,52 @@ const HOST_OS_CODE: u8 = 3;
 /// Try to connect to the configured HID device(s)
 /// and send HID messages passing the current host OS code
 pub fn main() -> anyhow::Result<()> {
+    if !rusb::has_hotplug() {
+        anyhow::bail!("No hotplug compat");
+    }
     let mut config_path = dirs::config_local_dir().context("Could not find config path")?;
     config_path.push("keeb_os_probe.toml");
     let config_toml =
         fs::read_to_string(&config_path).context(format!("Config path: {:?}", &config_path))?;
     let config: Config = toml::from_str(&config_toml)?;
-    let board_connection = BoardConnection::new(&config)?;
-    board_connection.probe()
+    let board_connection = BoardConnection::new(config)?;
+    let context = rusb::Context::new()?;
+    let _reg = rusb::HotplugBuilder::new()
+        .enumerate(true)
+        .register::<rusb::Context, _>(&context, Box::new(board_connection))?;
+    loop {
+        context.handle_events(None)?;
+    }
 }
 
-struct BoardConnection<'a> {
-    hid_api: hidapi::HidApi,
-    config: &'a Config,
+#[derive(Debug, Deserialize)]
+struct Config {
+    keyboards: HashMap<String, KeyboardConfig>,
 }
-impl<'a> BoardConnection<'a> {
-    pub fn new(config: &'a Config) -> anyhow::Result<Self> {
+
+#[derive(Debug, Deserialize)]
+struct KeyboardConfig {
+    vendor_id: u16,
+    product_id: u16,
+}
+
+struct BoardConnection {
+    hid_api: hidapi::HidApi,
+    config: Config,
+}
+impl BoardConnection {
+    pub fn new(config: Config) -> anyhow::Result<Self> {
         Ok(Self {
             hid_api: hidapi::HidApi::new()?,
             config,
         })
     }
 
-    pub fn probe(&self) -> anyhow::Result<()> {
-        for (keeb, keeb_config) in &self.config.keyboards {
+    pub fn probe(&self, vendor_id: u16, product_id: u16) -> anyhow::Result<()> {
+        if let Some((keeb, keeb_config)) = &self.config.keyboards.iter().find(|(_, keeb_config)| {
+            keeb_config.vendor_id == vendor_id && keeb_config.product_id == product_id
+        }) {
+            thread::sleep(Duration::from_millis(50));
             let Some(device) = self.hid_api.device_list().find(|device| {
                 device.vendor_id() == keeb_config.vendor_id
                     && device.product_id() == keeb_config.product_id
@@ -75,4 +88,14 @@ impl<'a> BoardConnection<'a> {
         }
         Ok(())
     }
+}
+impl<T: rusb::UsbContext> rusb::Hotplug<T> for BoardConnection {
+    fn device_arrived(&mut self, device: rusb::Device<T>) {
+        if let Ok(desc) = device.device_descriptor() {
+            self.probe(desc.vendor_id(), desc.product_id())
+                .expect("Probed device");
+        }
+    }
+
+    fn device_left(&mut self, _device: rusb::Device<T>) {}
 }
